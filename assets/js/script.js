0f04ff4f9088f06530556e31160de7d4a8f17445
Muhammad Nursalli <shallimuhammad@gmail.com>
2022-04-03 12:26:55 +0700

    Membuat Logika Permainan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Kondisi Awal vs dan Hasil Permainan
 const vs = document.getElementById('vs')
 vs.style.display = 'block'
 
@@ -9,8 +10,10 @@ const pilihanComputer = document.querySelectorAll('.pilihanComputer')
 
 pilihanPlayer.forEach((pp) => {
     pp.addEventListener('click', () => {
+        //Memberi Style Pilihan Player 1
         pp.classList.add('pilihan')
-
+        
+        //Memberi Animasi Acak Pada Pilihan Computer
         let waktu = 100;
         for(let i = 0; i < pilihanComputer.length; i++){
             setTimeout(() => {
@@ -25,14 +28,89 @@ pilihanPlayer.forEach((pp) => {
             }
         }
 
+        //Memberi Animasi Hasil Permainan
         setTimeout(() => {
-            vs.style.display = 'none'
+            //Ambil Pilihan Player dan Computer
+            const hasilPP = pp.getAttribute('id')
+            let hasilPC = Math.floor( Math.random() * pilihanComputer.length + 1 )
 
             const hasil1 = document.querySelector('#hasilPermainan h1:nth-child(1)')
             const hasil2 = document.querySelector('#hasilPermainan h1:nth-child(2)')
 
-            hasil1.innerHTML = 'Player 1'
-            hasil2.innerHTML = 'Lose'
+            //Menampilkan Hasil Permainan
+            vs.style.display = 'none'
+            pilihanComputer[hasilPC - 1].classList.add('pilihan')
+
+            //Mencari Pilihan Computer
+            switch(hasilPC){
+                case 1:
+                    hasilPC = 'batu'
+                    break
+                case 2:
+                    hasilPC = 'kertas'
+                    break
+                case 3:
+                    hasilPC = 'gunting'
+                    break
+                default:
+                    hasilPC = 'Error'
+                    break
+            }
+            
+            //Logika Aturan Permainan
+
+            //Cek Style Selain Draw
+            const cekOpacity = hasilPermainan.classList.contains('bg-opacity-50')
+
+            if(hasilPP == hasilPC){
+                hasil1.style.display = 'none'
+                hasil2.innerHTML = 'Draw'
+                hasilPermainan.classList.remove('bg-opacity-50')
+            } else if(hasilPP == 'batu') {
+                if (hasilPC == 'gunting') {
+                    hasil1.style.display = 'block'
+                    hasil1.innerHTML = 'PLAYER 1' 
+                    hasil2.innerHTML = 'Win'
+
+                    if (!cekOpacity) hasilPermainan.classList.add('bg-opacity-50')
+                } else {
+                    hasil1.style.display = 'block'
+                    hasil1.innerHTML = 'COM'
+                    hasil2.innerHTML = 'Win'
+
+                    if (!cekOpacity) hasilPermainan.classList.add('bg-opacity-50')
+                }
+            } else if(hasilPP == 'gunting'){
+                if (hasilPC == 'batu') {
+                    hasil1.style.display = 'block'
+                    hasil1.innerHTML = 'COM'
+                    hasil2.innerHTML = 'Win'
+
+                    if (!cekOpacity) hasilPermainan.classList.add('bg-opacity-50')
+                } else{
+                    hasil1.style.display = 'block'
+                    hasil1.innerHTML = 'PLAYER 1'
+                    hasil2.innerHTML = 'Win'
+                    
+                    if (!cekOpacity) hasilPermainan.classList.add('bg-opacity-50')
+                }
+            } else if(hasilPP == 'kertas'){
+                if (hasilPC == 'batu') {
+                    hasil1.style.display = 'block'
+                    hasil1.innerHTML = 'PLAYER 1'
+                    hasil2.innerHTML = 'Win'
+
+                    if (!cekOpacity) hasilPermainan.classList.add('bg-opacity-50')
+                } else {
+                    hasil1.style.display = 'block'
+                    hasil1.innerHTML = 'COM'
+                    hasil2.innerHTML = 'Win'
+
+                    if (!cekOpacity) hasilPermainan.classList.add('bg-opacity-50')
+                }
+            } else {
+                return 'error'
+            }
 
             hasilPermainan.style.display = 'block'
             hasilPermainan.classList.add('hasil')
@@ -40,6 +118,7 @@ pilihanPlayer.forEach((pp) => {
     })
 });
 
+//Mengembalikan Kondisi Semula
 const refresh = document.getElementById('ulang')
 
 refresh.addEventListener('click', () => {
@@ -53,4 +132,5 @@ refresh.addEventListener('click', () => {
 
     vs.style.display = 'block'
     hasilPermainan.style.display = 'none'
+    hasilPermainan.classList.remove('hasil')
 })
